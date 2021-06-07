e5ac6a0c065feaa71edbff1e23a4f9e3e591bfb2
Mohit Kumar <mohitkumaris@gmail.com>
2021-06-07 15:39:59 +0530

    used google Oauth

---
+++ src/components/streams/StreamEdit.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StreamEdit = () => {
+  return <div>List</div>;
+};
+
+export default StreamEdit;
---
+++ src/reducers/authReducer.js
@@ -0,0 +1,18 @@
+const INITIAL_STATE = {
+  isSignedIn: null,
+  userID: null,
+};
+
+const authReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case "SIGN_IN":
+      return { ...state, isSignedIn: true, userID: action.payload };
+    case "SIGN_OUT":
+      return { ...state, isSignedIn: false, userID: null };
+
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
